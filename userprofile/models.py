5f7d4e7480e0038c846275e8c3c9b5c58241f13a
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-19 21:10:06 +0600

    Created view and url

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 
 class authUserProfile(User):
     phone = models.CharField(max_length=15, null=True, blank=True)
-    image_link = models.ImageField(upload_to='/user-image', blank=True, null=True)
+    image_link = models.ImageField(upload_to='user-image', blank=True, null=True)
     
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.auth_user_profile, name='user_profile')
+    path('accounts/', views.auth_user_profile, name='user_profile')
 ]
