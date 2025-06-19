e74b4a6e57a3d7be0051234f7beb3fef8fe7e48e
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-20 00:28:02 +0600

    Profile created

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('/logout', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile/', views.user_profile, name='user_profile'),
     path('register/', views.register, name='register'),    
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,15 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import UserProfile
 
 
 
 
 
+# Genral way
+# @login_required
+# def user_profile(request):
+#     profile = request.user.userprofile
+#     return render(request, 'accounts/profile.html', {'profile': profile})
 
 
+# Best way
 @login_required
 def user_profile(request):
-    profile = request.user.userprofile
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
     return render(request, 'accounts/profile.html', {'profile': profile})
 
 
