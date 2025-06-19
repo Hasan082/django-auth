eb45430f6bf12916ce2f4dd7e7fcb750454addeb
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-20 02:35:04 +0600

    register done

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.user_profile, name='user_profile'),
-    path('register/', views.register, name='register'),    
+    path('register/', views.register, name='register'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),   
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import UserProfile
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
 from django.views.decorators.cache import never_cache
 
-
+from django.contrib.auth.models import User
+from .forms import UserRegistrationForm
 
 
 
@@ -32,4 +33,29 @@ def logout_view(request):
 
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            # Create UserProfile
+            # UserProfile.objects.create(
+            #     user=user,
+            #     phone=form.cleaned_data['phone'],
+            #     image_link=form.cleaned_data['image_link']
+            # )
+            
+            # Update auto-created UserProfile
+            profile = user.userprofile
+            profile.phone = form.cleaned_data['phone']
+            profile.image_link = form.cleaned_data['image_link']
+            profile.save()
+
+            login(request, user)  # Log the user in after registering
+            return redirect('user_profile')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
