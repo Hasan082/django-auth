c8bd4d58a23da089c5bfd1ef244a4ee209ec292f
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-19 21:00:13 +0600

    Created view and url

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class authUserProfile(User):
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    image_link = models.ImageField(upload_to='/user-image', blank=True, null=True)
+    
